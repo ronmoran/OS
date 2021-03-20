71ef6b3d4194bfcbb91152d85bfc89ac9a2b9903
Ron Moran <nomail@nomail.com>
2021-03-20 20:57:17 +0200

    with argument

--- ex1/sandbox.cpp
+++ ex1/sandbox.cpp
@@ -2,11 +2,13 @@
 // Created by Ron on 3/20/2021.
 //
 #include "osm.cpp"
-int main() {
-    printf("Time for addition %.2f nanoseconds", osm_operation_time(105));
+#include <stdlib.h>
+int main(int argc, char* argv[]) {
+    int iterations = strtol(argv[1], NULL, 10);
+    printf("Time for addition %.2f nanoseconds", osm_operation_time(iterations));
     printf("\n");
-    printf("Time for function %.2f nanoseconds", osm_function_time(105));
+    printf("Time for function %.2f nanoseconds", osm_function_time(iterations));
     printf("\n");
-    printf("Time for syscall %.2f nanoseconds", osm_syscall_time(105));
+    printf("Time for syscall %.2f nanoseconds", osm_syscall_time(iterations));
     return 0;
 }
