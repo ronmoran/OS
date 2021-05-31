045499d56749421702b123089ea84ee814f77b7b
Noi Maymaran <noimimran@cs.huji.ac.il>
2021-05-31 22:32:43 +0300

    creepy messages wanted a backup

--- ex3/MapReduceFramework.cpp
+++ ex3/MapReduceFramework.cpp
@@ -3,6 +3,7 @@
 #include <algorithm>
 #include <pthread.h>
 #include <semaphore.h>
+#include <iostream>
 #include "MapReduceFramework.h"
 #include "Barrier/Barrier.h"
 
@@ -12,23 +13,15 @@ typedef uint64_t UINT64;
 #define COUNTER_WIDTH 31
 #define TOTAL_TASKS_WIDTH 31
 #define STAGE_WIDTH 2
-
+class JobContext;
 bool weak_order(const IntermediatePair &pair1, const IntermediatePair &pair2)
 {
     return pair1.first < pair2.first;
 }
 
-//todo emit2
-//todo emit3
 
 
-struct ThreadContext
-{
-    pthread_t *thread;
-    IntermediateVec intermediate; //each thread's intermediate vector
-    uint32_t threadID;
-    JobContext* jobContext; //holds all other refrences
-};
+
 
 /**
  * Update the phase of the map reduce between different stage_t values
@@ -39,7 +32,14 @@ void updatePhase(std::atomic<UINT64> *stage, stage_t st)
 {
     stage->exchange(*stage & (MAX_UINT64 & ((UINT64)st<<COUNTER_WIDTH<<TOTAL_TASKS_WIDTH)));
 }
-
+struct ThreadContext
+{
+    pthread_t *thread;
+    IntermediateVec intermediate; //each thread's intermediate vector
+    uint32_t threadID;
+    JobContext* jobContext; //holds all other refrences
+    int multiThreadLevel;
+};
 class JobContext {
 private:
     ThreadContext *threads;
@@ -49,7 +49,7 @@ private:
     // 2 MSB is stage_t (map, shuffle, undefined...) 31 LSB are the counter
     std::atomic<UINT64> stage;
     Barrier barrier;
-    sem_t* shuffleSem;
+    sem_t shuffleSem;
 
     static void* runThread(void* thisObj)
     {
@@ -65,14 +65,33 @@ private:
         j->barrier.barrier();
         if (tc -> threadID != 0)
         {
-            sem_wait(j->shuffleSem); //stop all threads but the shuffling thread (zero)
+            sem_wait(&j->shuffleSem); //stop all threads but the shuffling thread (zero)
         }
         else
         {
             updatePhase(&(j->stage), SHUFFLE_STAGE);
             //todo shuffle
+            bool isEmpty = false;
+            unsigned long numPairs = tc->jobContext->threads->intermediate.size();
+            IntermediateVec* intermediateVectors = new IntermediateVec[numPairs];
+            while(!isEmpty)
+            {
+                IntermediateVec newAssignment;
+                for(int k = 0; k < tc->multiThreadLevel; k++)
+                {
+                    auto currThreadIntermediate = tc->jobContext->threads[k].intermediate;
+                    if (currThreadIntermediate.empty()){
+                        isEmpty = true;
+                        break;
+                    }
+                    IntermediatePair topPair = currThreadIntermediate.back();
+                    newAssignment.push_back(topPair);
+                    currThreadIntermediate.pop_back();
+                }
+                intermediateVectors[numPairs-tc->jobContext->threads->intermediate.size()] = newAssignment;
+            }
         }
-        sem_post(j->shuffleSem);
+        sem_post(&j->shuffleSem);
         //todo reduce
         return nullptr;
     }
@@ -81,16 +100,19 @@ private:
 public:
     JobContext(const MapReduceClient& client,
                const InputVec& inputVec, OutputVec& outputVec,
-               int multiThreadLevel): client(client), input(inputVec), output(outputVec), stage(UNDEFINED_STAGE),
-               barrier(multiThreadLevel)
+               int multiThreadLevel): client(client), input(inputVec), output(outputVec), stage(UNDEFINED_STAGE)
+               ,barrier(multiThreadLevel)
     {
-        threads = new ThreadContext[multiThreadLevel]();
-        sem_init(shuffleSem, 0, 0);
+        threads = new ThreadContext[multiThreadLevel];
+        threads->multiThreadLevel = multiThreadLevel;
+        sem_init(&shuffleSem, 0, 0);
         for(uint32_t i = 0; i < multiThreadLevel; i++)
         {
             threads[i].threadID = i;
             threads[i].jobContext = this;
-            pthread_create(threads[i].thread, nullptr, JobContext::runThread, static_cast<void*>(threads + i));
+            pthread_t threadId;
+//            pthread_create(threads[i].thread, nullptr, &JobContext::runThread, static_cast<void*>(threads + i));
+            pthread_create(&threadId, nullptr, &JobContext::runThread, static_cast<void*>(threads + i));
         }
     }
     ~JobContext()
@@ -98,3 +120,43 @@ public:
         delete[] threads;
     }
 };
+
+
+
+JobHandle startMapReduceJob(const MapReduceClient& client,
+                            const InputVec& inputVec, OutputVec& outputVec,
+                            int multiThreadLevel){
+    JobContext* jc = new JobContext(client, inputVec, outputVec, multiThreadLevel);
+
+}
+
+/**
+ * The function receives as input intermediary element (K2, V2) and context which contains
+    data structure of the thread that created the intermediary element. The function saves the
+    intermediary element in the context data structures. In addition, the function updates the
+    number of intermediary elements using atomic counter.
+ * @param key
+ * @param value
+ * @param context
+ */
+void emit2 (K2* key, V2* value, void* context){
+
+    auto *tc = static_cast<ThreadContext*>(context);
+    tc->intermediate.push_back(IntermediatePair(key, value));
+    std::cout<<1;
+    //todo increment intermid counter
+    return;
+}
+void emit3 (K3* key, V3* value, void* context){
+    return;
+}
+void waitForJob(JobHandle job){
+
+}
+void getJobState(JobHandle job, JobState* state){
+
+}
+void closeJobHandle(JobHandle job){
+
+}
+
